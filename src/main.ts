d73f983155756ab81e599378c85b19dd370d4c94
FructDev <luisfructuoso86@gmail.com>
2025-07-11 00:57:52 -0400

    repair Cords problem

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,10 @@ async function bootstrap() {
   // NOTA PROFESIONAL: Habilitar CORS (Cross-Origin Resource Sharing)
   // Permite que nuestro frontend (que estará en un dominio diferente) se comunique con esta API.
   app.enableCors({
-    origin: true, // En producción, deberías poner aquí la URL de tu frontend
+    origin: [
+      'http://localhost:3000', // Para tu desarrollo local
+      'https://girasol-pwa.vercel.app', // Para tu app en producción
+    ], // En producción, deberías poner aquí la URL de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
